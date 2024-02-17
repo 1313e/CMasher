8f69c7646f300a47f542794c14351c7633143122
Clément Robert <cr52@protonmail.com>
2024-02-17 09:03:06 +0100

    REL: prepare release 1.8.0

--- cmasher/__version__.py
+++ cmasher/__version__.py
@@ -8,4 +8,4 @@ Stores the different versions of the *CMasher* package.
 
 # %% VERSIONS
 # Default/Latest/Current version
-__version__ = "1.7.2"  # keep in sync with pyproject.toml
+__version__ = "1.8.0"  # keep in sync with pyproject.toml
